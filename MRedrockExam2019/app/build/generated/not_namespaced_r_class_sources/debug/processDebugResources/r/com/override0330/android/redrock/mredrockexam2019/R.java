da128e813568126e90d0bc7d95224bc0e4a3f6ba
854098072@qq.com <854098072@qq.com>
2019-05-20 00:35:28 +0800

    change apk

--- MRedrockExam2019/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/override0330/android/redrock/mredrockexam2019/R.java
+++ MRedrockExam2019/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/override0330/android/redrock/mredrockexam2019/R.java
@@ -19031,4 +19031,7 @@ public final class R {
      */
     public static final int ViewStubCompat_android_inflatedId=2;
   }
+  public static final class xml {
+    public static final int network_security_config=0x7f110000;
+  }
 }
